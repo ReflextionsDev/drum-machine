7fecaf7dca36d8754666b03e2caba2dfd07ae312
ReflextionsDev <ReflextionsDev@gmail.com>
2021-11-16 12:14:54 -0500

    Part 1

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3')
+
+let tickCount = 0
+
+const tickCountText = document.querySelector('.count')
+
+
 
 // This function is called every 600ms
 function update() {
 
-    // Play the 'tick' sound
-    tick.play();
+    tickCount = tickCount >= 4 ? 1 : tickCount + 1
+    tickCountText.innerHTML = tickCount
+
+
+    tickCount === 4 ? tock.play() : tick.play()
 }
 
 // This function sets up update() to be called every 600ms
